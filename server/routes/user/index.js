452eb814323322600a957243a166971d5009b771
dooking <123456ghghgh@naver.com>
2021-07-12 14:23:33 +0900

    [feat] user list API

--- server/routes/user/index.js
+++ server/routes/user/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const controller = require('./controller')
 
-router.get('/', controller.getUsers)
+router.get('/list', controller.getUsers)
 router.post('/register', controller.registerUser)
 
 module.exports = router
