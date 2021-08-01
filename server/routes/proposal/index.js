9cacdf199b2f8305669205af252bf336948e0c74
dooking <123456ghghgh@naver.com>
2021-08-01 22:54:33 +0900

    [modify] proposal model 수정 및 proposal controller 순서 변경

--- server/routes/proposal/index.js
+++ server/routes/proposal/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router()
 const controller = require('./controller')
 
+router.get('/list', controller.getProposals)
+router.post('/register', controller.registerProposal)
 router.get('/:id', controller.getProposal)
 router.patch('/:id', controller.editProposal)
 router.delete('/:id', controller.deleteProposal)
-router.get('/list', controller.getProposals)
-router.post('/register', controller.registerProposal)
 
 router.get('/user/:userId', controller.getProposalByUserId)
 
--- server/services/db/proposal.js
+++ server/services/db/proposal.js
@@ -1,36 +1,37 @@
 const { Proposal } = require('../../models')
 const { Keyword } = require('../../models')
 
-exports.selectProposal = async (proposalId) =>  
+exports.selectProposal = async (proposalId) =>
   await Proposal.findOne({
     raw: true,
     where: {
       id: proposalId,
     },
-  }).then((results) => {
-    console.log("Success Selecting Proposal");
-    return results
   })
-  .catch((err) => {
-    console.log("Failed Selecting Proposal");
-    return err
-  })
+    .then((results) => {
+      console.log('Success Selecting Proposal')
+      return results
+    })
+    .catch((err) => {
+      console.log('Failed Selecting Proposal')
+      return err
+    })
 
-
-exports.selectProposalByUserId = async (userId) =>  
+exports.selectProposalByUserId = async (userId) =>
   await Proposal.findOne({
     raw: true,
     where: {
       user_id: userId,
     },
-  }).then((results) => {
-    console.log("Success Selecting Proposal");
-    return results
   })
-  .catch((err) => {
-    console.log("Failed Selecting Proposal");
-    return err
-  })
+    .then((results) => {
+      console.log('Success Selecting Proposal')
+      return results
+    })
+    .catch((err) => {
+      console.log('Failed Selecting Proposal')
+      return err
+    })
 
 exports.updateProposal = async ({
   id,
@@ -41,52 +42,59 @@ exports.updateProposal = async ({
   distance,
   description,
   status,
-}) =>  await Proposal.update({
-    raw: true,
-    before_src,
-    after_src,
-    price_limit,
-    distance,
-    description,
-    status
-  }, {
-    where: {
-      id: id
+}) =>
+  await Proposal.update(
+    {
+      raw: true,
+      before_src,
+      after_src,
+      price_limit,
+      distance,
+      description,
+      status,
+    },
+    {
+      where: {
+        id: id,
+      },
     }
-  }).then((results) => {
-    console.log("Success Updating Proposal");
-    return results
-  })
-  .catch((err) => {
-    console.log("Failed Updating Proposal");
-    return err
-  })
+  )
+    .then((results) => {
+      console.log('Success Updating Proposal')
+      return results
+    })
+    .catch((err) => {
+      console.log('Failed Updating Proposal')
+      return err
+    })
 
-exports.destroyProposal = async (proposalId) =>  
+exports.destroyProposal = async (proposalId) =>
   await Proposal.destroy({
     where: {
       id: proposalId,
     },
-  }).then((results) => {
-    console.log("Success Destroying Proposal");
-    return results
   })
-  .catch((err) => {
-    console.log("Failed Destroying Proposal");
-    return err
-  })
+    .then((results) => {
+      console.log('Success Destroying Proposal')
+      return results
+    })
+    .catch((err) => {
+      console.log('Failed Destroying Proposal')
+      return err
+    })
 
-exports.selectAllProposal = async () =>  
+exports.selectAllProposal = async () =>
   await Proposal.findAll({
     raw: true,
-  }).then((results) => {
-    console.log("Success Selecting All Proposal");
-    return results
   })
-  .catch((err) => {
-    console.log("Failed Selecting All Proposal");
-    return err
-  })
+    .then((results) => {
+      console.log('Success Selecting All Proposal')
+      return results
+    })
+    .catch((err) => {
+      console.log('Failed Selecting All Proposal')
+      return err
+    })
 
 exports.insertProposal = async ({
   user_id,
@@ -97,7 +105,8 @@ exports.insertProposal = async ({
   keywords,
   description,
   status,
-}) => Proposal.create({
+}) =>
+  Proposal.create({
     raw: true,
     user_id,
     before_src,
@@ -107,11 +116,12 @@ exports.insertProposal = async ({
     keywords,
     description,
     status,
-  }).then((results) => {
-    console.log("Success Creating Proposal");
-    return results
   })
-  .catch((err) => {
-    console.log("Failed Creating Proposal");
-    return err
-  })
+    .then((results) => {
+      console.log('Success Creating Proposal')
+      return results
+    })
+    .catch((err) => {
+      console.log('Failed Creating Proposal')
+      return err
+    })
