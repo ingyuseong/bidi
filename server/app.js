387c943c528288cd8b4065f06a5732c7ae7595e4
staycozyboy <dkc984@likelion.org>
2021-08-16 01:26:57 +0900

    [refactor] socket directory 삭제 및 socket.io PORT 4000으로 server와 통합

--- server/app.js
+++ server/app.js
@@ -6,6 +6,20 @@ const { sequelize } = require('./models')
 const app = express()
 const PORT = process.env.PORT
 
+// socket.io configuration
+const socketPORT = 4000;
+
+// const server = require("http").createServer();
+const server = require("http").Server(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+// Type
+const NEW_CHAT_MESSAGE_EVENT = "newChatMessage";
+
 sequelize
   .sync({ alter: false })
   .then(() => console.log('DB 연결 성공!'))
@@ -24,5 +38,28 @@ app.use('/api', apiRouter)
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
 })
+
+// Start socket.io server on port 4000
+io.on("connection", (socket) => {
+  
+  // Join a conversation
+  const { roomId } = socket.handshake.query;
+  socket.join(roomId);
+  console.log("socket.io server: Connected: " + roomId);
+
+  // Listen for new messages
+  socket.on(NEW_CHAT_MESSAGE_EVENT, (data) => {
+    io.in(roomId).emit(NEW_CHAT_MESSAGE_EVENT, data);
+  });
+
+  // Leave the room if the user closes the socket
+  socket.on("disconnect", () => {
+    socket.leave(roomId);
+  });
+});
+
+server.listen(socketPORT, () => {
+  console.log(`socket.io server: Listening on port ${socketPORT}`);
+});
 
 module.exports = app
