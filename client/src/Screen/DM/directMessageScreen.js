00992c1aebf556fb15e682d9810cd374b39767dd
staycozyboy <dkc984@likelion.org>
2021-08-15 17:09:01 +0900

    [chore] Clean up useless codes

--- client/src/Screen/DM/directMessageScreen.js
+++ client/src/Screen/DM/directMessageScreen.js
@@ -7,11 +7,7 @@ import {
   ScrollView,
 } from 'react-native';
 
-// import { socket } from '../../Common/socket';
-// import { io } from 'socket.io-client';
-
 import useChat from './useChat';
-
 import ChatBubbleList from '../../Components/DM/chatBubbleList';
 import DMHeader from '../../Components/DM/dMHeader';
 
@@ -21,57 +17,13 @@ function DMScreen({ navigation, route }) {
 
   const roomId = user['id'];
   const [ messages, SendMessage ] = useChat(roomId);
-  console.log(SendMessage);
   
   const [messageText, setMessageText] = useState('');
-  // const [messages, setMessages] = useState(dummyMessages);
-  
-  // const socketURL = 'http://localhost:3001';
-
-  // const socket = io.connect(socketURL, {
-  //   transports: ['websocket']
-  // });
-
-  // const newUserConnected = (userName) => {
-  //   socket.emit("new user", userName);
-  // };
 
   const handleSubmit = (event) => {
-    // event.preventDefault();
-    // let newMessages = [...messages, {
-    //   userId: 1,
-    //   customerSent: true,
-    //   content: messageText,
-    //   createdAt: '2021-07-15 08:44:45',
-    // }]
-    // setMessages(newMessages);
-    // socket.emit('message text', messageText);
-    // setMessageText('');
-    
     SendMessage(messageText);
     setMessageText('');
   }
-
-  // useEffect(() => {
-  //   newUserConnected();
-
-  //   socket.on("new user", function (data) {
-  //     data.map((user) => {console.log(`New user Connected: ${user}`)});
-  //   });
-
-  //   socket.on("user disconnected", function (userName) {
-  //     console.log(`New user Connected: ${userName}`)
-  //   });
-
-  //   socket.on("message text", msg => {
-  //     setMessages([...messages, {
-  //       userId: 1,
-  //       customerSent: true,
-  //       content: msg,
-  //       createdAt: '2021-07-15 08:44:45',
-  //     }]);
-  //   });
-  // }, []);
 
   // Header style configuration
   useLayoutEffect(() => {
