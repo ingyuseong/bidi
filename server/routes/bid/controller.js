f5ea5041cc3deeded8beebe66bf4f4abbb750007
dooking <123456ghghgh@naver.com>
2021-08-28 14:02:15 +0900

    [refactor] bid 작성 시, styles 정봋 ㅜ가

--- server/routes/bid/controller.js
+++ server/routes/bid/controller.js
@@ -5,17 +5,23 @@ const { STATUS_CODE, ERROR_MESSAGE } = require('../../lib/constants')
 exports.registerBid = async (req, res, next) => {
   try {
     const params = req.body
-    console.log('body', params)
     const bid = await bidServices.createBid(params)
-    console.log('bid', bid.id)
     const bidStyle = await bidServices.createBidStyle({
       bidId: bid.id,
       styles: params.styles,
     })
-    res.status(STATUS_CODE.SUCCESS).json({
-      message: '비드 작성 성공',
-      data: { bid, bidStyle },
-    })
+    if (bid || bidStyle) {
+      const data = { ...bid.dataValues }
+      data.styles = params.styles
+      res.status(STATUS_CODE.SUCCESS).json({
+        message: '비드 작성 성공',
+        data,
+      })
+    } else {
+      res
+        .status(STATUS_CODE.SERVER_ERROR)
+        .json({ message: ERROR_MESSAGE.SERVER_ERROR })
+    }
   } catch (error) {
     console.log(error)
     res
