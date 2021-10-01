7522952e2fdb541e56c8429c35f9cab221ae176f
staycozyboy <dkc984@likelion.org>
2021-10-01 21:01:22 +0900

    feat: RabbitMQ result queue listener 추가

---
+++ server/lib/listenForResults.js
@@ -0,0 +1,46 @@
+const amqp = require('amqplib')
+
+exports.listenForResults = async () => {
+  // connect to Rabbit MQ
+  let connection = await amqp.connect(process.env.AMQP_URL)
+
+  // create a channel and prefetch 1 message at a time
+  let channel = await connection.createChannel()
+  await channel.prefetch(1)
+
+  // start consuming messages
+  console.log('Start listening for results queue of RabbitMQ')
+  await consume({ connection, channel })
+}
+
+function consume({ connection, channel, resultsChannel }) {
+  return new Promise((resolve, reject) => {
+    channel.consume('processing.results', async function (msg) {
+      // parse message
+      let msgBody = msg.content.toString()
+      console.log(msgBody)
+      let data = JSON.parse(msgBody)
+      let requestId = data.requestId
+      let processingResults = data.requestData
+      console.log(
+        'Received a result message, requestId:',
+        requestId,
+        'processingResults:',
+        processingResults
+      )
+
+      // acknowledge message as received
+      await channel.ack(msg)
+    })
+
+    // handle connection closed
+    connection.on('close', (err) => {
+      return reject(err)
+    })
+
+    // handle errors
+    connection.on('error', (err) => {
+      return reject(err)
+    })
+  })
+}
