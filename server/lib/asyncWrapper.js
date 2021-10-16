f156811b40336d030f54d580a20a2579d0df83ef
jiwonny <whdwhd115@korea.ac.kr>
2021-10-16 21:19:03 +0900

    [feat] error catch할 때 슬랙으로 알림 전송

--- server/lib/asyncWrapper.js
+++ server/lib/asyncWrapper.js
@@ -1,9 +1,22 @@
+const { slack } = require('./slack')
+
+const reorganizeErrorMessage = (req, err) => {
+  return JSON.stringify({
+    ip: req.ip,
+    url: req.originalUrl,
+    errorCode: err.code,
+    errorMessage: err.message,
+    errorStack: err.stack
+  }, null, 2)
+}
+
 exports.routesAsyncWrapper = (asyncFn) => async (req, res, next) => {
   try {
     return await asyncFn(req, res, next)
   } catch (err) {
     console.error(err.message)
     console.error(err.stack) // console logging이 아닌 elastic search 등의 툴 추가 필요!
+    slack.writeLogAndSendAlert(reorganizeErrorMessage(req, err))
     next(err) // error를 error handling middleware로 전송
   }
 }
